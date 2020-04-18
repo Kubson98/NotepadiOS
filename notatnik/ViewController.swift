ea9cefbdd4d4e1b90e36ca7cd470b4f81a24aa1e
Kubson98 <sedal.jakub@gmail.com>
2020-04-18 12:39:22 +0200

    Initial Commit

---
+++ notatnik/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  notatnik
+//
+//  Created by Kuba on 18/04/2020.
+//  Copyright © 2020 Kuba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
